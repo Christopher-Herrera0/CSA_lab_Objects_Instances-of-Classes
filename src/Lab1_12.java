0031564af6795ff1951de14b2105ac9d6d77b673
PandeJoker <cherrera27237@columbushs.com>
2025-10-01 11:32:42 -0400

    Done

--- src/Lab1_12.java
+++ src/Lab1_12.java
@@ -1,5 +1,6 @@
-
+import java.awt.Point;
 public class Lab1_12 {
+    
     public static void main(String[] args) {
         System.out.println("Task 1: " + q1());
         System.out.println("Task 2: " + q2());
@@ -9,25 +10,35 @@ public class Lab1_12 {
 
     // 1. Create Point (5,10) and return coordinates
     public static String q1() {
-        // TODO
-        return "";
+        Point p1 = new Point(5,10);
+        return "(" + ((int) p1.getX()) + ", " + ((int) p1.getY()) + ")";
     }
 
     // 2. Move a Point (0,0) to (4,7) and return coordinates
     public static String q2() {
-        // TODO
-        return "";
+        Point p2 = new Point(0,0);
+        p2.move(4, 7);
+
+        return "(" + ((int) p2.getX()) + ", " + ((int) p2.getY()) + ")";
     }
 
     // 3. Find distance between (0,0) and (3,4)
     public static double q3() {
-        // TODO
-        return 0.0;
+        Point p3 = new Point(0,0);
+        Point p4 = new Point(3,4);
+
+        return Point.distance(p3.getX(), p3.getY(), p4.getX(), p4.getY());
     }
 
     // 4. Show that two Points with same values are different objects
     public static String q4() {
-        // TODO
-        return "";
+        Point p5 = new Point(1,6);
+        Point p6 = new Point(1, 6);
+
+        boolean Value = (p5 == p6);
+        boolean Memory = (p5.equals(p6));
+        
+
+        return Value + ", " + Memory;
     }
 }
